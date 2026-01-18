1f20f2ac64edb5b13798007f83c6f2c754280236
brodeck <raphael.donikian@outlook.fr>
2026-01-18 01:35:15 +0100

    phase 0 : liaison depot laptop bis

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Description of AccueilController
+ * Description of AccueilController test
  *
  * @author emds
  */
