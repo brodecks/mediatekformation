557d8e2bc977b74bbd3236f69cd989f16d65c1f6
Brodecks <raphae.donikian@oulootk.fr>
2026-03-21 03:19:35 +0100

    Mission 1 : Nettoyage du code et ajout nb formations

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -20,6 +20,7 @@ class FormationsController extends AbstractController {
      * @var FormationRepository
      */
     private $formationRepository;
+    private const CHEMIN_FORMATIONS = "pages/formations.html.twig";
     
     /**
      * 
@@ -36,7 +37,7 @@ class FormationsController extends AbstractController {
     public function index(): Response{
         $formations = $this->formationRepository->findAll();
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::CHEMIN_FORMATIONS, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -46,7 +47,7 @@ class FormationsController extends AbstractController {
     public function sort($champ, $ordre, $table=""): Response{
         $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::CHEMIN_FORMATIONS, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -57,7 +58,7 @@ class FormationsController extends AbstractController {
         $valeur = $request->get("recherche");
         $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::CHEMIN_FORMATIONS, [
             'formations' => $formations,
             'categories' => $categories,
             'valeur' => $valeur,
